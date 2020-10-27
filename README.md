e28d6346b7de1f549a23f6baece120c94b5ec1f5
Igor Magollo <igraphmagollo@gmail.com>
2020-10-27 03:00:57 -0300

    add feed crud

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Hackernews-node
 
-How to graphql tutorial app 
+Hacker News clone
--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
-    }
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+        link: (parent, args) => links.find(element => element.id == args.id)
+    },
+
+    Mutation: {
+      post: (parent, args) => {
+        const link = {
+          id: `link-${idCount++}`,
+          description: args.description,
+          url: args.url,
+        }
+        links.push(link)
+        return link
+      },
+      updateLink: (parent, args) => {
+        const index = links.findIndex(element => element.id == args.id)
+        if (index === -1)
+          return undefined
+        if (args.url !== undefined)
+          links[index].url = args.url
+        if (args.description !== undefined)
+          links[index].description = args.description
+        return links[index]
+      },
+      deleteLink: (parent, args) => {
+        const index = links.findIndex(element => element.id == args.id)
+        if (index === -1)
+          return undefined
+        const link = links[index]
+        links.splice(index, 1)
+        return link
+      }
+    },
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 
